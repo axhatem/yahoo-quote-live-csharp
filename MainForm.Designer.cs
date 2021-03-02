df678c82b7f9fdc034ddea55a3f085536e940364
Axel <axelensiie@gmail.com>
2021-03-02 15:54:59 -0500

    First version

---
+++ MainForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace YahooQuotesLive
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubscribeToSymbol = new System.Windows.Forms.Button();
+            this.tbxYahooSymbol = new System.Windows.Forms.TextBox();
+            this.lblYahooSymbol = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSubscribeToSymbol
+            // 
+            this.btnSubscribeToSymbol.Location = new System.Drawing.Point(263, 21);
+            this.btnSubscribeToSymbol.Name = "btnSubscribeToSymbol";
+            this.btnSubscribeToSymbol.Size = new System.Drawing.Size(169, 29);
+            this.btnSubscribeToSymbol.TabIndex = 0;
+            this.btnSubscribeToSymbol.Text = "Subsribe to quotes";
+            this.btnSubscribeToSymbol.UseVisualStyleBackColor = true;
+            this.btnSubscribeToSymbol.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbxYahooSymbol
+            // 
+            this.tbxYahooSymbol.Location = new System.Drawing.Point(121, 23);
+            this.tbxYahooSymbol.Name = "tbxYahooSymbol";
+            this.tbxYahooSymbol.Size = new System.Drawing.Size(125, 27);
+            this.tbxYahooSymbol.TabIndex = 2;
+            // 
+            // lblYahooSymbol
+            // 
+            this.lblYahooSymbol.AutoSize = true;
+            this.lblYahooSymbol.Location = new System.Drawing.Point(12, 26);
+            this.lblYahooSymbol.Name = "lblYahooSymbol";
+            this.lblYahooSymbol.Size = new System.Drawing.Size(103, 20);
+            this.lblYahooSymbol.TabIndex = 3;
+            this.lblYahooSymbol.Text = "Yahoo symbol";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 458);
+            this.Controls.Add(this.lblYahooSymbol);
+            this.Controls.Add(this.tbxYahooSymbol);
+            this.Controls.Add(this.btnSubscribeToSymbol);
+            this.Name = "MainForm";
+            this.Text = "Yahoo live quote displayer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubscribeToSymbol;
+        private System.Windows.Forms.TextBox tbxYahooSymbol;
+        private System.Windows.Forms.Label lblYahooSymbol;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,94 @@
+﻿using Google.Protobuf;
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Websocket.Client;
+
+namespace YahooQuotesLive
+{
+    public partial class MainForm : Form
+    {
+        private delegate void SetTextDelegate(Label ticker, string labelValue, Label quote, string quoteValue, Color color);
+
+        private Label _lastLabelTicker;
+        private Label _lastLabelQuote;
+        private MessageParser<YahooQuote> _parser;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _parser = new MessageParser<YahooQuote>(() => new YahooQuote());
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            CenterToScreen();
+        }
+
+        private void Update(Label ticker, string tickerValue, Label quote, string quoteValue, Color color)
+        {
+            ticker.Text = tickerValue;
+            quote.Text = quoteValue;
+            quote.ForeColor = color;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (_lastLabelTicker == null)
+            {
+                _lastLabelTicker = new Label();
+                _lastLabelTicker.Location = new Point(lblYahooSymbol.Location.X, lblYahooSymbol.Location.Y + lblYahooSymbol.Size.Height + 20);
+            }
+            else
+            {
+                var previousLabelTicker = _lastLabelTicker;
+                _lastLabelTicker = new Label();
+                _lastLabelTicker.Location = new Point(previousLabelTicker.Location.X, previousLabelTicker.Location.Y + previousLabelTicker.Size.Height + 20);
+            }
+
+            _lastLabelQuote = new Label();
+            _lastLabelQuote.Location = new Point(_lastLabelTicker.Location.X + 60, _lastLabelTicker.Location.Y);
+            _lastLabelQuote.Size = new Size(500, _lastLabelQuote.Size.Height);
+
+            Controls.Add(_lastLabelQuote);
+            Controls.Add(_lastLabelTicker);
+
+            string yahooSymbol = tbxYahooSymbol.Text.ToUpper();
+
+            await Task.Factory.StartNew(() =>
+            {
+                var lastLabelTicker = _lastLabelTicker;
+                var lastLabelQuote = _lastLabelQuote;
+
+                using (var client = new WebsocketClient(new Uri("wss://streamer.finance.yahoo.com")))
+                {
+                    client.ReconnectTimeout = TimeSpan.FromSeconds(10);
+                    client.MessageReceived.Subscribe(msg =>
+                    {
+                        byte[] data = Convert.FromBase64String(msg.Text);
+                        var quote = _parser.ParseFrom(data);
+                        string quoteText = DisplayQuote(quote);                        
+                        Invoke(new SetTextDelegate(Update),
+                            lastLabelTicker,
+                            yahooSymbol,
+                            lastLabelQuote,
+                            quoteText,
+                            quote.ChangePercent > 0 ? Color.Green : Color.Red);
+                    });
+                    client.Start();
+                    client.Send(@"{ ""subscribe"": [""[TICKER]""] }".Replace("[TICKER]", yahooSymbol));
+                    new ManualResetEvent(false).WaitOne();
+                }
+            });
+        }
+
+        private string DisplayQuote(YahooQuote quote)
+        {
+            return $"{Math.Round(quote.Price, 2)} {Math.Round(quote.Change, 2)} ({Math.Round(quote.ChangePercent, 2)}%)";
+        }        
+    }
+}
